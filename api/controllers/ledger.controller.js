6e0ceb6cf13f44ae541e465f0b139341efdb878d
NishithHM <nishithhm@Apples-MacBook-Pro.local>
2022-03-13 13:23:01 +0530

    added valid slot validation

--- api/controllers/ledger.controller.js
+++ api/controllers/ledger.controller.js
@@ -1,6 +1,7 @@
 const Ledgers = require('../models/leger.model')
 const Services = require('../models/services.model')
 const dayjs = require('dayjs');
+const { isEqual } = require('lodash');
 
 async function isBooked({date, startTime, endTime, serviceID}) {
     const bookedSlots = await Ledgers.count(
@@ -11,8 +12,16 @@ async function isBooked({date, startTime, endTime, serviceID}) {
             serviceID,
         })
     const service = await Services.findOne({serviceID:serviceID })
-	console.log(service.appointment)
-    return bookedSlots >= service.appointment.maxAppointmentPerSlot
+	const startTimeVal = dayjs(startTime, 'YYYY-MM-DD HH:mm').format('HH:mm')
+	const endTimeVal = dayjs(endTime, 'YYYY-MM-DD HH:mm').format('HH:mm')
+	const requestedSlot = {begin:startTimeVal, end:endTimeVal}
+	const slots= service?.appointment?.timeSlots
+	const trimmedSlot = slots.map(ele=> {
+		const {begin, end} = ele
+		return {begin, end}
+	})
+	const isInValidSlot = !trimmedSlot.some(ele=> isEqual(ele, requestedSlot))
+    return bookedSlots >= service.appointment.maxAppointmentPerSlot || isInValidSlot
 }
 
 exports.bookTheSlot = async (req, res) => {
@@ -26,7 +35,7 @@ exports.bookTheSlot = async (req, res) => {
         res.status(201).send("booked successfully")
 		}
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         res.status(500).send(error)
     }
 
