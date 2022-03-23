c43e6212234e91e0ccb44728b363377b3926fba8
Charith RC <Charith.RC@novigo.com>
2022-03-23 21:44:49 +0530

    admin statistics  based on filters added V1

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -5,25 +5,45 @@ const User = require('../models/user.model')
 const LedgerHandler = require('../controllers/ledger.controller')
 const Ledgers = require('../models/leger.model')
 
-
-
-
 exports.adminStats = async (req, res) => {
 	// testing Phase
-	
-
-	var queryObj;
-	if(req.body.userId!= null){
-		queryObj = {userId:req.body.userId}
+	var queryObj = new Object()
+	if (req.body.userId != null) {
+		queryObj.userId = req.body.userId;
 	}
-	queryObj+={isBooked:true}
+	if (req.body.serviceId != null) {
+		queryObj.serviceId = req.body.serviceId;
+	}
+	if (req.body.startTime != null) {
+		queryObj.startTime = req.body.startTime;
+	}
+	if (req.body.endTime != null) {
+		queryObj.endTime = req.body.endTime;
+	}
+	if (req.body.isBooked != null) {
+		queryObj.isBooked = req.body.isBooked
+	}
+	if (req.body.bookedBy != null) {
+		queryObj.bookedBy = req.body.bookedBy;
+	}
+	if (req.body.createdAt != null) {
+		queryObj.createdAt = req.body.createdAt;
+	}
+	if (req.body.updatedAt != null) {
+		queryObj.updatedAt = req.body.updatedAt;
+	}
+
+	// if(req.body. != null){
+	// 	queryObj. = req.body.
+	// }
 	console.log(queryObj)
-	const ledger = await Ledgers.find( queryObj );
-	console.log(ledger)
-	res.status(201).send(ledger)
-	//upload.single('Image')
-	// const image = req.file;
-	// console.log(image)
+	try {
+		const ledger = await Ledgers.find(queryObj);
+		console.log(ledger)
+		res.status(201).send(ledger)
+	} catch (error) {
+		res.status(500).send(error)
+	}
 }
 
 
--- api/controllers/ledger.controller.js
+++ api/controllers/ledger.controller.js
@@ -5,88 +5,90 @@ const { isEqual } = require('lodash');
 const utc = require('dayjs/plugin/utc')
 dayjs.extend(utc)
 
-async function isBooked({startTime, endTime, serviceId}) {
-   try {
-	const bookedSlots = await Ledgers.count(
-        {
-            startTime,
-			endTime,
-            serviceId,
-        })
-    const service = await Services.findById(serviceId)
-	
-	if(service == null){
-		return true;
+async function isBooked({ startTime, endTime, serviceId }) {
+	try {
+		const bookedSlots = await Ledgers.count(
+			{
+				startTime,
+				endTime,
+				serviceId,
+			})
+		const service = await Services.findById(serviceId)
+
+		if (service == null) {
+			return true;
+		}
+		const startTimeVal = dayjs(startTime).format('HH:mm')
+		const endTimeVal = dayjs(endTime).format('HH:mm')
+
+		// const startTimeVal1 = dayjs(dayjs.utc(startTime)).format('HH:mm')
+		// const endTimeVal1 = dayjs(dayjs.utc(endTime)).format('HH:mm')
+		// console.log(startTimeVal1,endTimeVal1)
+		const requestedSlot = { begin: startTimeVal, end: endTimeVal }
+		const slots = service?.appointment?.timeSlots
+		const trimmedSlot = slots.map(ele => {
+			const { begin, end } = ele
+			return { begin, end }
+		})
+		const isInValidSlot = !trimmedSlot.some(ele => isEqual(ele, requestedSlot))
+		return bookedSlots >= service.appointment.maxAppointmentPerSlot || isInValidSlot
+
+	} catch (error) {
+		return error
 	}
-	const startTimeVal = dayjs(startTime).format('HH:mm')
-	const endTimeVal = dayjs(endTime).format('HH:mm')
-	
-	// const startTimeVal1 = dayjs(dayjs.utc(startTime)).format('HH:mm')
-	// const endTimeVal1 = dayjs(dayjs.utc(endTime)).format('HH:mm')
-	// console.log(startTimeVal1,endTimeVal1)
-	const requestedSlot = {begin:startTimeVal, end:endTimeVal}
-	const slots= service?.appointment?.timeSlots
-	const trimmedSlot = slots.map(ele=> {
-		const {begin, end} = ele
-		return {begin, end}
-	})
-	const isInValidSlot = !trimmedSlot.some(ele=> isEqual(ele, requestedSlot))
-    return bookedSlots >= service.appointment.maxAppointmentPerSlot || isInValidSlot
-	   
-   } catch (error) {
-	   return error
-   }
 }
 
 exports.bookTheSlot = async (req, res) => {
-    try {
-        const ledger = new Ledgers(req.body);
-        ledger.userId = req.body.userId;
+	try {
+		const ledger = new Ledgers(req.body);
+		ledger.userId = req.body.userId;
 		ledger.bookedBy = req.user._id;
-		
-        if (await isBooked(req.body)) {
-           res.status(403).send("Selected slot not available")
-        }else{
-        await ledger.save();
-        res.status(201).send("booked successfully")
+
+		if (await isBooked(req.body)) {
+			res.status(403).send("Selected slot not available")
+		} else {
+			await ledger.save();
+			res.status(201).send("booked successfully")
 		}
-    } catch (error) {
-        	 console.log(error)
-        res.status(500).send(error)
-    }
+	} catch (error) {
+		console.log(error)
+		res.status(500).send(error)
+	}
 }
 
-exports.getAvailableSlots = async (req, res)=>{
-		const {serviceId, date} = req.query
-		console.log(serviceId, date)
-		const service = await Services.findById(serviceId)
-		console.log(service)
-		if(service){
+exports.getAvailableSlots = async (req, res) => {
+	const { serviceId, date } = req.query
+	console.log(serviceId, date)
+	const service = await Services.findById(serviceId)
+	console.log(service)
+	if (service) {
 		const slots = service.appointment.timeSlots
-		const trimmedSlot = slots.map(ele=> {
-			const {begin, end} = ele
-			return {startTime: dayjs(`${date} ${begin}`).format('YYYY-MM-DD HH:mm'),
-			 endTime:dayjs(`${date} ${end}`).format('YYYY-MM-DD HH:mm'), isBooked:true}
+		const trimmedSlot = slots.map(ele => {
+			const { begin, end } = ele
+			return {
+				startTime: dayjs(`${date} ${begin}`).format('YYYY-MM-DD HH:mm'),
+				endTime: dayjs(`${date} ${end}`).format('YYYY-MM-DD HH:mm'), isBooked: true
+			}
 		})
 		const availableRes = []
-		for(let i=0; i< trimmedSlot.length; i++){
-		const bookedCount = await Ledgers.count(trimmedSlot[i])
+		for (let i = 0; i < trimmedSlot.length; i++) {
+			const bookedCount = await Ledgers.count(trimmedSlot[i])
 			availableRes.push({
 				...trimmedSlot[i],
 				remainingSlots: service?.appointment?.maxAppointmentPerSlot - bookedCount
 			})
-	   }
-	res.json({slots:availableRes, serviceId})
-	}else{
-		res.json({slots:[], serviceId})
+		}
+		res.json({ slots: availableRes, serviceId })
+	} else {
+		res.json({ slots: [], serviceId })
 	}
 }
-exports.CancelSlot = async (req,res)=>{
+exports.CancelSlot = async (req, res) => {
 
 	try {
-		const ledger = await Ledgers.findByIdAndUpdate(req.body.ledgerId,{isBooked:false},{new: true})
+		const ledger = await Ledgers.findByIdAndUpdate(req.body.ledgerId, { isBooked: false }, { new: true })
 		res.status(201).send(ledger)
-	
+
 	} catch (error) {
 		res.status(500).send(error)
 	}
