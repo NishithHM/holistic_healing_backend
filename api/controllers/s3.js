98e4cbc01357d3b1d513db9a080ea58e0451f27e
NishithHM <nishithhm@ip-192-168-0-109.ap-south-1.compute.internal>
2022-04-06 10:53:53 +0530

    fixed

--- api/controllers/s3.js
+++ api/controllers/s3.js
@@ -1,11 +1,16 @@
 const AWS = require('aws-sdk');
 const fs = require('fs')
 require('dotenv').config();
-
-const region = process.env.AWS_BUCKET_REGION;
-const bucketName= process.env.AWS_BUCKET_NAME;
-const accessKey = process.env.AWS_ACCESS_KEY;
-const secretKey = process.env.AWS_SECRET_KEY;
+/* 
+Access Key ID:
+AKIAVQ2J72GQU4W2YBNP
+Secret Access Key:
+gMxkN6VG6Pf6sOA92ufi/03e/9MiPJ4lmMAITa9h
+*/
+const region = process.env.AWS_BUCKET_REGION || 'ap-south-1';
+const bucketName= process.env.AWS_BUCKET_NAME || 'holistichealing';
+const accessKey = process.env.AWS_ACCESS_KEY || 'AKIAVQ2J72GQU4W2YBNP';
+const secretKey = process.env.AWS_SECRET_KEY || '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';
 
 const s3 = new AWS.S3({
     region,
@@ -26,7 +31,7 @@ async function uploadFile(file) {
         console.log("Successfully uploaded to " + bucketName + "/" + key);
     }).catch(
       function(err) {
-        console.error(err, err.stack);
+        console.error(err, err.stack, 'jere');
     });
 
   }
