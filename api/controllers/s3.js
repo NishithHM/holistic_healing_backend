db17823d1bdcb498daa744d514c27dcc37fd045c
Charith RC <Charith.RC@novigo.com>
2022-04-06 20:45:20 +0530

    keysHandling -1

--- api/controllers/s3.js
+++ api/controllers/s3.js
@@ -1,30 +1,25 @@
 const AWS = require('aws-sdk');
 const fs = require('fs')
 const S3= require('aws-sdk/clients/s3')
-require('dotenv').config();
-
-const region = process.env.AWS_BUCKET_REGION;
-const bucketName= process.env.AWS_BUCKET_NAME;
-const accessKey = process.env.AWS_ACCESS_KEY;
-const secretKey = process.env.AWS_SECRET_KEY;
-//console.log(region)
+const Keys = require('../keys')
+const {AWS_BUCKET_REGION,AWS_BUCKET_NAME,AWS_ACCESS_KEY,AWS_SECRET_KEY} = Keys
 
 const s3 = new S3({
-
+  accessKeyId:AWS_ACCESS_KEY,
+  secretAccessKey:AWS_SECRET_KEY
 })
 
 async function uploadFile(file) {
     const fileStream = fs.createReadStream(file.path)
    
     const uploadParams = {
-      Bucket: "holistichealing",
+      Bucket: AWS_BUCKET_NAME,
       Key: file.filename,
       Body: fileStream
-     
     }
     s3.putObject(uploadParams).promise().then(
       function(data) {
-        console.log("Successfully uploaded to " + "sd" + "/" );
+        console.log("Successfully uploaded to " + AWS_BUCKET_NAME  );
     }).catch(
       function(err) {
         console.error(err, err.stack);
---
+++ api/keys.js
@@ -0,0 +1,10 @@
+var Keys = {
+
+    AWS_BUCKET_REGION :"ap-south-1",
+    AWS_BUCKET_NAME :"holistichealing",
+    AWS_ACCESS_KEY :"AKIAVQ2J72GQZYCKIY5W",
+    AWS_SECRET_KEY :"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",
+
+}
+
+module.exports = Keys 
